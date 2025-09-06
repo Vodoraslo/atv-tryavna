46e84bad85c2717f78dc3484aa0680d5da0e058a
Nikoleta Nikolaeva <nikoletadamyanova2002@gmail.com>
2025-09-06 18:00:43 +0300

    menu

--- script.js
+++ script.js
@@ -51,3 +51,9 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
   
+  const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
